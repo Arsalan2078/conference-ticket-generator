b04ae6f04085f9f91548b7a636a17953415d76a1
Arsalan Rabdanov <arsalan.rabdanov2078@gmail.com>
2025-07-12 09:15:00 -0400

    style guide implemeted

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import "./App.css";
+import classes from "./App.module.scss";
 
 export default function App() {
-  return <>App</>;
+  return (
+    <div className={`${classes.app} text-preset-4`}>
+      The quick brown fox jumps over the lazy dog.
+    </div>
+  );
 }
